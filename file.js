61efa0f80aa6845f6b0e2561b039d1ada0c6c7d2
beribaeva <99745915+beribaeva@users.noreply.github.com>
2024-10-03 02:17:43 -0400

    added a two pointer algorithm

--- file.js
+++ file.js
@@ -1,1 +1,17 @@
+var checkifPalindrome = function(s){
+    let left = 0;
+    let right = s.length-1;
+    while(left<right){
+        if (s[left] != s[right]){
+        return false;
+        }
+        left++;
+        right --;
+    }
+    
+    return true; 
+}
 
+// Time complexity = O(n)
+// Space complexity = O(1) -> No matter how big the input we will never use more than two variables in this algorithm
+//This algorithm works with both arrays and strings
